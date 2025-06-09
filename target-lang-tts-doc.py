ac8f7b11ccdf395543cf9304b36bbfee270765f7
ukduw <edhkmayo@gmail.com>
2025-06-09 14:52:19 +0100

    tech stack/notes

---
+++ target-lang-tts-doc.py
@@ -0,0 +1,16 @@
+
+# scrape pdf, skip tables/figures...
+
+# ElevenLabs multilingual model
+# TTS engine + bilingual AI voice map
+    # configs: pitch, speech, pauses, volume, etc...
+
+# tkinter GUI?
+    # ideally, user can select/deselect desired parts from pdf
+
+# python machine translation - EasyNMT?
+# use dictionary of most common words; use inverse to identify specialist/technical language
+    # sparsely translate, label as lang
+# translate (some) full sentences below a certain length (basic sentences)
+
+# 
