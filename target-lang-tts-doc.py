c65e0fdc41738ca953fbbd30b71c91ce89d09466
ukduw <edhkmayo@gmail.com>
2025-08-02 12:32:16 -0400

    updated plans/notes on machine translation

--- target-lang-tts-doc.py
+++ target-lang-tts-doc.py
@@ -7,8 +7,16 @@
 # 2
 # python machine translation - EasyNMT?
 # use dictionary of most common words; use inverse to identify specialist/technical language
-    # sparsely translate, label as lang
+    # sparsely translate, label as lang, put original in brackets
 # translate (some) full sentences below a certain length (basic sentences)
+# update: a lot of new syntax... bit unsure of how to do it without hard-coding...
+    # maybe determine total number of words + short sentences to be translated by the total length of the report
+    # use these totals to limit a list to a max length
+    # populate list with strings - most uncommon vocab (determined via dictionary of most common words) + sentences below x length
+    # iterate through the list, machine translating each and adding the translations to another list...
+    # replace original with structure "translation (original text)"
+        # ...how can the position of the original be identified???
+        # maybe they can be replaced with a jibberish marker as they're added to list...
 
 # 3
 # would rather set up periodic scrape/checks
