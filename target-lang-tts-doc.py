66f0f0e002616b6e3783f213681b496ea11671cb
ukduw <edhkmayo@gmail.com>
2025-08-02 11:51:24 -0400

    updated plans/notes, categorized by module/function

--- target-lang-tts-doc.py
+++ target-lang-tts-doc.py
@@ -1,16 +1,20 @@
 
-# scrape pdf, skip tables/figures...
-
+# 1
 # ElevenLabs multilingual model
 # TTS engine + bilingual AI voice map
     # configs: pitch, speech, pauses, volume, etc...
 
-# tkinter GUI?
-    # ideally, user can select/deselect desired parts from pdf
-
+# 2
 # python machine translation - EasyNMT?
 # use dictionary of most common words; use inverse to identify specialist/technical language
     # sparsely translate, label as lang
 # translate (some) full sentences below a certain length (basic sentences)
 
-# 
+# 3
+# would rather set up periodic scrape/checks
+# two methods:
+    # some are very straightforward, with weekly updates...
+    # others do not seem to have a set schedule
+        # may have to store the latest title as string
+        # each scrape iterates through titles, stopping when it reaches the stored on
+
