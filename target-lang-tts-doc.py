0c69142315e22536e1719fbfc3d7d1e2bdef0c64
ukduw <edhkmayo@gmail.com>
2025-08-02 11:59:15 -0400

    added details on insights reports, sources, barriers to scrape... ordered by least to most complicated

--- target-lang-tts-doc.py
+++ target-lang-tts-doc.py
@@ -18,3 +18,23 @@
         # may have to store the latest title as string
         # each scrape iterates through titles, stopping when it reaches the stored on
 
+
+
+# LEAST to MOST complicated sources:
+# 1) Deloitte - Weekly global economic update
+    # 1/week, weds, all html
+    # most straightforward and consistent by far
+    # https://www.deloitte.com/us/en/insights/topics/economy/global-economic-outlook/weekly-update/weekly-update-2025-06.html
+# 2) McKinsey & Company - Featured Insights
+    # weekly updates, all html, sorted into categories
+        # "explainers", "week in charts", "themes", "lives & legacies"
+        # each article labeled - easy to ignore categories that tend to be chart/infographic/link-heavy
+    # non-weekly updates - large pdf reports
+        # more difficult, more charts, etc...
+    # https://www.mckinsey.com/featured-insights
+
+# 3) 
+# 4) 
+
+
+# checked other language sources... not what i'm looking for
